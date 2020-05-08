e4cfba0c1c9b37958aed41a5903d3ef695eb7dbe
krozic <kyle.rozic@gmail.com>
2020-05-08 17:47:50 -0400

    assignment completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculating the inverse of a matrix can be a costly operation.
+## If an inverse is recquired for multiple operations the inverse
+## can be "cached" by storing it in a function environment 
+## that can be recalled from memory at a later time.
 
-## Write a short comment describing this function
+## The makeCacheMatrix() function takes a matrix 'x' as an input
+## and returns a special matrix that contains the set(), get(),
+## setinv(), and getinv() functions to be used to recall and/or
+## store the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_m <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv_m <<- inverse
+  getinv <- function() inv_m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve() function takes the output of makeCacheMatrix()
+## as an input argument. If !is.null(inv_m) is TRUE, the function 
+## retrieves the 'inv_m' value from the parent environment and 
+## returns the value. 
+## If !is.null(inv_m) is FALSE, the matrix is retrieved
+## with x$get(), the inverse is calculated and stored in 'inv_m', 
+## and the value of inv_m is stored in the parent environment with
+## x$setinv(inv_m). The function then returns inv_m.
+## To change the matrix to a 'newmatrix' without calling 
+## makeCacheMatrix() again, myMatrix$set(newmatrix) can be called to store
+## the new matrix in the parent environment.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv_m <- x$getinv()
+  if(!is.null(inv_m)) {
+    message("getting cached data")
+    return(inv_m)
+  }
+  data <- x$get()
+  inv_m <- solve(data, ...)
+  x$setinv(inv_m)
+  inv_m
 }
